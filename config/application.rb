06d5815efde766df8e686caee84c0e36ef931f16
syo tokeshi <tokeshi@mfgnt.com>
2023-01-15 20:31:32 +0900

    railsを日本時間に対応 + アセットファイル等はrails generate時に自動生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,14 @@ Bundler.require(*Rails.groups)
 
 module ProfessionalBaseballOwners
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false          # CSS, JavaScriptファイルを自動生成しない
+      g.helper     false      # helperファイルを自動生成しない
+    end  
   end
 end
